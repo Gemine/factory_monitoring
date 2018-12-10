4f6133636345b626d3b0edf2004888d0ea504e55
Phuc <nguyenxuanphuc6794@gmail.com>
2018-12-10 14:11:52 +0700

    code for 40

---
+++ 40.ino
@@ -0,0 +1,155 @@
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* ssid = "Tenda_5EF570";
+const char* password = "";
+const char* mqtt_server = "192.168.1.105";
+const char* clientID = "40";
+const char* topic="cokhi/40/status";
+
+WiFiClient espClient;
+
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+char speedString[10];
+int value = 0;
+int D5st=0,D6st=0,D7st=0;
+double Aostt = 0.0,sp=0.0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(D5,INPUT);
+  pinMode(D6,INPUT);
+  pinMode(D7,INPUT);
+  
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  IPAddress ip(192, 168, 1, 129);
+  IPAddress gateway(192, 168, 1, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.config(ip, gateway, subnet);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientID)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }  
+  //D5st= digitalRead(D5);
+  //D6st= digitalRead(D6);
+  //D7st= digitalRead(D7);
+  long now = millis();
+  if (now - lastMsg > 1000) {
+    lastMsg = now;
+    Aostt=analogRead(A0);
+    if(Aostt<=20.0){
+      D5st=1;
+      D6st=0;
+      D5st=1;
+    };
+    if(Aostt>20.0){
+      D5st=0;
+      D6st=1;
+      D5st=1;
+    };
+    
+    
+    dtostrf(sp,2,2,speedString);
+    
+    snprintf (msg, 75, "ON:%ld,OFF:%ld,ERR:%ld,SPEED:%lf", D5st,D6st,D7st,Aostt);
+    //snprintf (msg, 75, "speed:%ld", Aostt);
+    Serial.print("Publish message: ");
+    Serial.println(Aostt);
+    Serial.println(D5st);
+    Serial.println(D6st);
+    Serial.println(D7st);
+    client.publish(topic, msg);
+    
+  }
+  client.loop();
+}
